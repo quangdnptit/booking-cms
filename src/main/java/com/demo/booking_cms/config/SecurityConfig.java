c9f8a5ae2ebf8f3d27da5b6922587d610c54bd26
quang <quangat333@gmail.com>
2026-03-10 23:39:50 +0700

    add swagger ui

--- src/main/java/com/demo/booking_cms/config/SecurityConfig.java
+++ src/main/java/com/demo/booking_cms/config/SecurityConfig.java
@@ -25,7 +25,7 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/auth/**").permitAll()
+                        .requestMatchers("/api/auth/**", "/", "/swagger-ui/**", "/v3/api-docs/**").permitAll()
                         .requestMatchers("/error").permitAll()
                         .anyRequest().authenticated()
                 )
